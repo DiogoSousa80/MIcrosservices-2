f7837807dbd9bfa599910a6349e57437155b0576
sousa <sousa@DIOGO-SOUSA>
2021-08-22 18:51:42 -0300

    Adicionar arquivos de projeto.

---
+++ Catalog.Api/Data/CatalogContext.cs
@@ -0,0 +1,42 @@
+﻿using Catalog.Api.Entities;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.Api.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+    
+    
+    public CatalogContext(IConfiguration confiration)
+        {
+            var client = new MongoClient(confiration.GetValue<string>
+                ("DatabaseSettings:ConnectionString"));
+            var database = client.GetDatabase(confiration.GetValue<string>
+                    ("DatabaseSettings:DatabaseName"));
+            Products = database.GetCollection<Product>(confiration.GetValue<string>
+                ("DatabaseSettings:ConnectionName"));
+
+            CatalogContextSeed.SeedData(Products);
+        }
+
+        public IMongoCollection<Product> Products { get; }
+
+        public IMongoCollection<Product> Product => throw new NotImplementedException();
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ Catalog.Api/Data/CatalogContextSeed.cs
@@ -0,0 +1,37 @@
+﻿using Catalog.Api.Entities;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.Api.Data
+{
+    public class CatalogContextSeed
+    {
+        private static object productCollection;
+
+        public CatalogContextSeed()
+        {
+        }
+
+     public static void SeedData(IMongoCollection<Product>mongoCollection)
+        {
+            bool existProduct = productCollection.find(p => true)
+                .Any();
+            if (!existProduct)
+
+            {
+                object productCollection = null;
+                object p = productCollection.InsertManyAsybc(GetMyProducts());
+            }
+                    
+                    
+       }
+
+        private static object GetMyProducts()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
